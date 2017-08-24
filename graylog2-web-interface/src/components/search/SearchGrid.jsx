113dbf14151ae4518dcd0cc4beb3010e91d95fa3
Dennis Oelkers <dennis@graylog.com>
2017-08-24 09:35:38 +0200

    Retrieving results for all queries in store.

--- graylog2-web-interface/src/components/search/SearchGrid.jsx
+++ graylog2-web-interface/src/components/search/SearchGrid.jsx
@@ -4,13 +4,40 @@ import { ReactGridContainer } from 'components/common';
 import { QuerySidebar, SearchWidget } from 'components/search';
 import style from 'pages/ShowDashboardPage.css';
 
+import CombinedProvider from 'injection/CombinedProvider';
+const { SearchStore } = CombinedProvider.get('Search');
+const { UniversalSearchStore } = CombinedProvider.get('UniversalSearch');
+
 const SearchGrid = React.createClass({
   getInitialState() {
     return {
       positions: { foo: { col: 0, row: 0, height: 3, width: 1.5 } },
+      searchResults: {},
     };
   },
-
+  componentDidMount() {
+    const { queryTree } = this.props;
+    this.retrieveQueryResults(queryTree);
+  },
+  componentWillReceiveProps(nextProps) {
+    const { queryTree } = nextProps;
+    this.retrieveQueryResults(queryTree);
+  },
+  retrieveQueryResults(queries) {
+    queries.forEach((query) => {
+        this.promise = UniversalSearchStore.search(SearchStore.originalRangeType, query.parameters.query, SearchStore.originalRangeParams.toJS(), null, null, SearchStore.page, SearchStore.sortField, SearchStore.sortOrder)
+          .then(
+            (response) => {
+              if (this.isMounted()) {
+                this.setState((state) => {
+                  state.searchResults[query.id] = response;
+                  state.error = undefined;
+                  return state;
+                });
+              }
+            });
+    });
+  },
   _onPositionsChange(positions) {
     const newPositions = {};
     positions.forEach(position => {
